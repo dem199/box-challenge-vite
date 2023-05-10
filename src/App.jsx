303e62b18cf8abda8b7420a8cc42ca34770afeae
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-05-10 21:51:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import boxes from "./boxes.jsx"
+import Box from "./Box.jsx"
+
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    function toggle(id) {
+        setSquares(prevSquares => {
+            return prevSquares.map((square) => {
+                return square.id === id ? Object.assign({}, square, {on: !square.on}) : square
+            })
+        })
+    }
+    
+    const squareElements = squares.map(square => (
+        <Box 
+            key={square.id} 
+            id={square.id}
+            on={square.on} 
+            toggle={toggle}
+        />
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
+
